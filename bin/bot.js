73bbd5836c15a35233f26ce10ff887a10ecd7f29
Seth Flynn <sethaflynn@gmail.com>
2020-11-14 13:16:42 -0500

    add a check for discord token

--- bin/bot.js
+++ bin/bot.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
-
 const redcord = require('../lib/redcord')
 const chalk = require('chalk')
 const Eris = require('eris')
 
+const ok = chalk.bold.cyan
+
+const bad = chalk.bold.red
+
+const link = chalk.underline.green
+
 const token = process.env.DISCORD_TOKEN
+
+if (token == null) {
+  console.log(bad('no discord token found as an enviormental variable. please set it as DISCORD_TOKEN.'))
+}
 
 const prefix = '-'
 
 const bot = new Eris(token)
-
-const ok = chalk.bold.cyan
-
-const link = chalk.underline.green
 
 let i = 0
 
